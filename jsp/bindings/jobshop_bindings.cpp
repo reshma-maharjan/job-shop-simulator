9327453cad36410a74a74c10e3ba2d09bb1ff381
Per-Arne Andersen <per@sysx.no>
2024-10-14 23:12:59 +0200

    Bindings update

--- jsp/bindings/jobshop_bindings.cpp
+++ jsp/bindings/jobshop_bindings.cpp
@@ -10,87 +10,100 @@
 #include "job_shop_actor_critic.h"
 #include "job_shop_taillard_generator.h"
 #include "job_shop_plotter.h"
+#include "job_shop_algorithms.h"
 
 namespace nb = nanobind;
 
 NB_MODULE(jobshop, m) {
+    // Bind Operation struct
+    nb::class_<Operation>(m, "Operation")
+        .def(nb::init<>())
+        .def_rw("duration", &Operation::duration)
+        .def_rw("machine", &Operation::machine)
+        .def_rw("eligibleMachines", &Operation::eligibleMachines)
+        .def_rw("dependentOperations", &Operation::dependentOperations);
+
     // Bind Job struct
     nb::class_<Job>(m, "Job")
-    .def(nb::init<>())
-    .def_rw("operations", &Job::operations)
-    .def_rw("machines", &Job::machines)
-    .def_rw("eligibleMachines", &Job::eligibleMachines);
+        .def(nb::init<>())
+        .def_rw("operations", &Job::operations)
+        .def_rw("dependentJobs", &Job::dependentJobs);
 
     // Bind Action struct
     nb::class_<Action>(m, "Action")
-    .def(nb::init<>())
-    .def(nb::init<int, int, int>())
-    .def_rw("job", &Action::job)
-    .def_rw("machine", &Action::machine)
-    .def_rw("operation", &Action::operation);
+        .def(nb::init<>())
+        .def(nb::init<int, int, int>())
+        .def_rw("job", &Action::job)
+        .def_rw("machine", &Action::machine)
+        .def_rw("operation", &Action::operation);
 
     // Bind State struct
     nb::class_<State>(m, "State")
-    .def(nb::init<int, int, int>())
-    .def_rw("jobProgress", &State::jobProgress)
-    .def_rw("machineAvailability", &State::machineAvailability)
-    .def_rw("nextOperationForJob", &State::nextOperationForJob);
+        .def(nb::init<int, int, int>())
+        .def_rw("jobProgress", &State::jobProgress)
+        .def_rw("machineAvailability", &State::machineAvailability)
+        .def_rw("nextOperationForJob", &State::nextOperationForJob)
+        .def_rw("completedJobs", &State::completedJobs)
+        .def_rw("jobStartTimes", &State::jobStartTimes);
 
     // Bind ScheduleEntry struct
     nb::class_<ScheduleEntry>(m, "ScheduleEntry")
-    .def(nb::init<>())
-    .def(nb::init<int, int, int>())
-    .def_rw("job", &ScheduleEntry::job)
-    .def_rw("start", &ScheduleEntry::start)
-    .def_rw("duration", &ScheduleEntry::duration)
-    .def("__getstate__", [](const ScheduleEntry &se) {
-        return std::make_tuple(se.job, se.start, se.duration);
-    })
-    .def("__setstate__", [](ScheduleEntry &se, const std::tuple<int, int, int> &state) {
-        new (&se) ScheduleEntry{
-            std::get<0>(state),
-            std::get<1>(state),
-            std::get<2>(state)
+        .def(nb::init<>())
+        .def(nb::init<int, int, int, int>())
+        .def_rw("job", &ScheduleEntry::job)
+        .def_rw("operation", &ScheduleEntry::operation)
+        .def_rw("start", &ScheduleEntry::start)
+        .def_rw("duration", &ScheduleEntry::duration)
+        .def("__getstate__", [](const ScheduleEntry &se) {
+            return std::make_tuple(se.job, se.operation, se.start, se.duration);
+        })
+        .def("__setstate__", [](ScheduleEntry &se, const std::tuple<int, int, int, int> &state) {
+            new (&se) ScheduleEntry{
+                std::get<0>(state),
+                std::get<1>(state),
+                std::get<2>(state),
+                std::get<3>(state)
             };
-    });
+        });
 
     // Bind JobShopEnvironment class
     nb::class_<JobShopEnvironment>(m, "JobShopEnvironment")
-    .def(nb::init<std::vector<Job>>())
-    .def("step", &JobShopEnvironment::step)
-    .def("reset", &JobShopEnvironment::reset)
-    .def("getState", &JobShopEnvironment::getState)
-    .def("getPossibleActions", &JobShopEnvironment::getPossibleActions)
-    .def("isDone", &JobShopEnvironment::isDone)
-    .def("getTotalTime", &JobShopEnvironment::getTotalTime)
-    .def("getJobs", &JobShopEnvironment::getJobs)
-    .def("getNumMachines", &JobShopEnvironment::getNumMachines)
-    .def("getScheduleData", &JobShopEnvironment::getScheduleData)
-    .def("printSchedule", &JobShopEnvironment::printSchedule);
+        .def(nb::init<std::vector<Job>>())
+        .def("step", &JobShopEnvironment::step)
+        .def("reset", &JobShopEnvironment::reset)
+        .def("getState", &JobShopEnvironment::getState)
+        .def("getPossibleActions", &JobShopEnvironment::getPossibleActions)
+        .def("isDone", &JobShopEnvironment::isDone)
+        .def("getTotalTime", &JobShopEnvironment::getTotalTime)
+        .def("getJobs", &JobShopEnvironment::getJobs)
+        .def("getNumMachines", &JobShopEnvironment::getNumMachines)
+        .def("getScheduleData", &JobShopEnvironment::getScheduleData)
+        .def("printSchedule", &JobShopEnvironment::printSchedule)
+        .def("generateOperationGraph", &JobShopEnvironment::generateOperationGraph);
 
     // Bind JobShopAlgorithm abstract class
     nb::class_<JobShopAlgorithm>(m, "JobShopAlgorithm")
-    .def("train", &JobShopAlgorithm::train)
-    .def("printBestSchedule", &JobShopAlgorithm::printBestSchedule)
-    .def("saveBestScheduleToFile", &JobShopAlgorithm::saveBestScheduleToFile);
+        .def("train", &JobShopAlgorithm::train)
+        .def("printBestSchedule", &JobShopAlgorithm::printBestSchedule)
+        .def("saveBestScheduleToFile", &JobShopAlgorithm::saveBestScheduleToFile);
 
     // Bind JobShopQLearning class
     nb::class_<JobShopQLearning, JobShopAlgorithm>(m, "JobShopQLearning")
-    .def(nb::init<JobShopEnvironment&, double, double, double>())
-    .def("runEpisode", &JobShopQLearning::runEpisode)
-    .def("train", &JobShopQLearning::train)
-    .def("printBestSchedule", &JobShopQLearning::printBestSchedule)
-    .def("saveBestScheduleToFile", &JobShopQLearning::saveBestScheduleToFile)
-    .def("applyAndPrintSchedule", &JobShopQLearning::applyAndPrintSchedule);
+        .def(nb::init<JobShopEnvironment&, double, double, double>())
+        .def("runEpisode", &JobShopQLearning::runEpisode)
+        .def("train", &JobShopQLearning::train)
+        .def("printBestSchedule", &JobShopQLearning::printBestSchedule)
+        .def("saveBestScheduleToFile", &JobShopQLearning::saveBestScheduleToFile)
+        .def("applyAndPrintSchedule", &JobShopQLearning::applyAndPrintSchedule);
 
     // Bind JobShopActorCritic class
     nb::class_<JobShopActorCritic, JobShopAlgorithm>(m, "JobShopActorCritic")
-    .def(nb::init<JobShopEnvironment&, double, double, double>())
-    .def("runEpisode", &JobShopActorCritic::runEpisode)
-    .def("train", &JobShopActorCritic::train)
-    .def("printBestSchedule", &JobShopActorCritic::printBestSchedule)
-    .def("saveBestScheduleToFile", &JobShopActorCritic::saveBestScheduleToFile)
-    .def("applyAndPrintSchedule", &JobShopActorCritic::applyAndPrintSchedule);
+        .def(nb::init<JobShopEnvironment&, double, double, double>())
+        .def("runEpisode", &JobShopActorCritic::runEpisode)
+        .def("train", &JobShopActorCritic::train)
+        .def("printBestSchedule", &JobShopActorCritic::printBestSchedule)
+        .def("saveBestScheduleToFile", &JobShopActorCritic::saveBestScheduleToFile)
+        .def("applyAndPrintSchedule", &JobShopActorCritic::applyAndPrintSchedule);
 
     // Bind TaillardInstance enum
     nb::enum_<TaillardInstance>(m, "TaillardInstance")
@@ -177,14 +190,15 @@ NB_MODULE(jobshop, m) {
 
     // Bind TaillardJobShopGenerator class
     nb::class_<TaillardJobShopGenerator>(m, "TaillardJobShopGenerator")
-    .def_static("loadProblem", &TaillardJobShopGenerator::loadProblem)
-    .def_static("verifyJobsData", &TaillardJobShopGenerator::verifyJobsData)
-    .def_static("verifyOptimalSolution", &TaillardJobShopGenerator::verifyOptimalSolution)
-    .def_static("runAllVerifications", &TaillardJobShopGenerator::runAllVerifications);
+        .def_static("loadProblem", &TaillardJobShopGenerator::loadProblem)
+        .def_static("verifyJobsData", &TaillardJobShopGenerator::verifyJobsData)
+        .def_static("verifyOptimalSolution", &TaillardJobShopGenerator::verifyOptimalSolution)
+        .def_static("runAllVerifications", &TaillardJobShopGenerator::runAllVerifications);
 
+    // Bind LivePlotter class
     nb::class_<LivePlotter>(m, "LivePlotter")
-    .def(nb::init<int>())
-    .def("render", &LivePlotter::render)
-    .def("updateSchedule", &LivePlotter::updateSchedule)
-    .def("shouldClose", &LivePlotter::shouldClose);
+        .def(nb::init<int>())
+        .def("render", &LivePlotter::render)
+        .def("updateSchedule", &LivePlotter::updateSchedule)
+        .def("shouldClose", &LivePlotter::shouldClose);
 }
